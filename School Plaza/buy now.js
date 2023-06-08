a632d8ce9d45e1d333ae74e6b1af26e2d4ca9719
Krishna Dhiman :) <125461682+Kri-warrior@users.noreply.github.com>
2023-06-08 22:08:57 +0530

    Add files via upload

---
+++ School Plaza/buy now.js
@@ -0,0 +1,11 @@
+function buy(){
+    alert("Your Order Is Placed")
+}
+
+function atc(){
+    alert("Your Order Is Added To The Cart")
+}
+
+function cart(){
+    alert("Add To Cart Coming Soon")
+}
